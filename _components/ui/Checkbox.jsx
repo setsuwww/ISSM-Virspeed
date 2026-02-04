ad287cf9e0850dcf6e0e00b0897986d949a81bc9
setsuwww <setsuprojectonly@gmail.com>
2026-02-04 21:35:29 +0700

    Stroke, Ring, Border heavier +Clear

--- _components/ui/Checkbox.jsx
+++ _components/ui/Checkbox.jsx
@@ -14,7 +14,7 @@ function Checkbox({
     <CheckboxPrimitive.Root
       data-slot="checkbox"
       className={cn(
-        "peer border-slate-500/30 ring ring-slate-200 data-[state=checked]:ring-indigo-600 data-[state=checked]:bg-indigo-500 border-0 data-[state=checked]:border-t data-[state=checked]:border-indigo-300 data-[state=checked]:text-white focus-visible:border-indigo-300 focus-visible:ring-indigo-300 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 size-4 shrink-0 rounded-[4px] outline-none disabled:cursor-not-allowed disabled:opacity-50",
+        "peer border-slate-500/50 ring ring-slate-300/90 data-[state=checked]:ring-indigo-600 data-[state=checked]:bg-indigo-500 border-0 data-[state=checked]:border-t data-[state=checked]:border-indigo-300 data-[state=checked]:text-white focus-visible:border-indigo-300 focus-visible:ring-indigo-300 aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 size-4 shrink-0 rounded-[4px] outline-none disabled:cursor-not-allowed disabled:opacity-50",
         "transition-transform duration-150 ease-out active:scale-110",
         className
       )}
--- _components/ui/Input.jsx
+++ _components/ui/Input.jsx
@@ -9,7 +9,7 @@ const sizeClasses = {
 };
 
 function Input({ className, type = "text", size = "md", typeSearch = false, typeDate = false, value, onChange, ...props }) {
-  
+
   const baseClasses = "file:text-slate-600 placeholder:text-slate-500 flex w-full min-w-0 rounded-lg border transition-[color,box-shadow,border] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50";
   const defaultFocus = "border-slate-300/60 bg-white focus-visible:border-slate-300 focus-visible:ring-[4px] focus-visible:ring-slate-100 shadow-2xs";
   const searchFocus = "border-slate-300/50 bg-slate-50/90 focus-visible:border-slate-300/50 focus-visible:ring-slate-100 caret-slate-400";
