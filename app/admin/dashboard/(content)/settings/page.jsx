6bc90743d8b75d32a0ddd7cfa6982e8007bd1246
setsuwww <setsuprojectonly@gmail.com>
2026-02-26 06:30:43 +0700

    foldern file

--- app/admin/dashboard/(content)/settings/page.jsx
+++
@@ -1,9 +0,0 @@
-import UpcomingStates from "@/_components/common/UpcomingStates";
-
-export default function SettingsPage() {
-    return (
-        <>
-            <UpcomingStates />
-        </>
-    )
-}
