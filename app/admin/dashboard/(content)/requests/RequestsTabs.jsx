ece35d82c2e8198bc5a4f274ba2d36796f2bbb10
setsuwww <setsuprojectonly@gmail.com>
2026-03-02 05:11:58 +0700

    Request handle

--- app/admin/dashboard/(content)/requests/RequestsTabs.jsx
+++ app/admin/dashboard/(content)/requests/RequestsTabs.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState } from "react"
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/_components/ui/Tabs"
+import { Tabs, TabsList, TabsTrigger } from "@/_components/ui/Tabs"
 import RequestsDataTable from "./RequestsTable"
 import { motion, AnimatePresence } from "framer-motion"
 
